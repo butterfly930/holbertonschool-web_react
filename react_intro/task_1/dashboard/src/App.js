4ddf092533a12b6ed8068d77d90c24d7d0e2bdce
butterfly930 <elsarrustaj@gmail.com>
2024-09-12 16:49:27 +0200

    commit to task 1

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import HolbertonLogo1 from "./HolbertonLogo1.png";
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils'
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={HolbertonLogo1} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
